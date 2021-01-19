332cc10d18768c1ce47baf369f357f510e7fefbc
Mete Çapar <mete.capar@ext.kocdigital.com>
2021-01-19 13:51:06 +0300

    Random user ureten database kullanmadan DotNet core ile API denemem

---
+++ UserManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.API.Fake;
+using UserManagement.API.Models;
+
+namespace UserManagement.API.Controllers
+{ 
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
+    {
+        private List<User> _users = FakeData.GetUsers(200);
+        [HttpGet]
+    public List<User> Get()
+    {
+        return _users;
+    }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        [HttpPut]
+        public User Put([FromBody]User user)
+        {
+            var editedUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            editedUser.Firstname = user.Firstname;
+            editedUser.Lastname = user.Lastname;
+            editedUser.Address = user.Address;
+            return user;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deletedUser = _users.FirstOrDefault(x => x.Id == id);
+            _users.Remove(deletedUser);
+        }
+
+
+    }
+}
---
+++ UserManagement.API/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.API.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Address { get; set; }
+    
+
+    }
+
+}
